61c554c69e3ab165182a295db9332833eebf85ee
GREATPERSON <168116588+greatpersonxd@users.noreply.github.com>
2024-05-04 13:34:19 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("INCRICIBLE")
+                await self.one.join_chat("THE_incricible")
             except:
                 pass
             assistants.append(1)
